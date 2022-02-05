02f6e635449d154e0d04d8adfd5cdcaa53933052
smradupan <martinelias630@gmail.com>
2022-02-05 16:48:39 +0100

    minor changes on functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,27 +9,25 @@ const addListeners = (array, callback) => {
     })
 };
 
-const showItem = (arrow, text) => {
-    text.style.fontWeight = `700`;
-    arrow.style.transform = `rotate(180deg)`;
-}
-
-const hideItem = (arrow, text) => {
-    text.style.fontWeight = `500`;
-    arrow.style.transform = `rotate(0deg)`;
-}
-
 const handleClick = (e) => {
     let clickedItem = e.target.closest('details');
-
     let arrow = clickedItem.querySelector('svg');
     let text = clickedItem.querySelector('summary');
 
     let clickedItemActive = clickedItem.attributes; // touching details element NamedNodeMap
-    let length = clickedItemActive.length;  // length of details NamedNodeMap
+
+    const showItem = (arrow, text) => {
+        text.style.fontWeight = `700`;
+        arrow.style.transform = `rotate(180deg)`;
+    }
+
+    const hideItem = (arrow, text) => {
+        text.style.fontWeight = `500`;
+        arrow.style.transform = `rotate(0deg)`;
+    }
 
     // condition when length == 0(details attribude open="") or when length == 1(details attribude open="")
-    if (length == 0) {
+    if (clickedItemActive.length == 0) {
         showItem(arrow, text)
     } else {
         hideItem(arrow, text)
