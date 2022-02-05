04e4cd8976f41e8ab5c2025de704622e76b8d9e0
smradupan <martinelias630@gmail.com>
2022-02-05 01:42:33 +0100

    template deleted, guide moved

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,39 @@
 'use strict';
-let summary = document.querySelectorAll('summary');
+const items = [...document.querySelectorAll('summary')];
 
-summary.forEach(item => {
-    item.addEventListener('click', (e) => {
-        let clickedItem = e.target.closest('details');
-        let arrow = clickedItem.querySelector('svg');
-        let text = clickedItem.querySelector('summary');
-
-        let clickedItemActive = clickedItem.attributes; // touching details element NamedNodeMap
-        let length = clickedItemActive.length;  // length of details NamedNodeMap
-
-        // condition when length == 0(details attribude open="") or when length == 1(details attribude open="")
-        if(length == 0) {
-            text.style.fontWeight = `700`;
-            arrow.style.transform = `rotate(180deg)`;
-        } else {
-            text.style.fontWeight = `500`;
-            arrow.style.transform = `rotate(0deg)`;
-        }
+const addListeners = (array, callback) => {
+    array.forEach(item => {
+        item.addEventListener('click', (e) => {
+            callback(e);
+        })
     })
-});
+};
+
+const showItem = (arrow, text) => {
+    text.style.fontWeight = `700`;
+    arrow.style.transform = `rotate(180deg)`;
+}
+
+const hideItem = (arrow, text) => {
+    text.style.fontWeight = `500`;
+    arrow.style.transform = `rotate(0deg)`;
+}
+
+const handleClick = (e) => {
+    let clickedItem = e.target.closest('details');
+
+    let arrow = clickedItem.querySelector('svg');
+    let text = clickedItem.querySelector('summary');
+
+    let clickedItemActive = clickedItem.attributes; // touching details element NamedNodeMap
+    let length = clickedItemActive.length;  // length of details NamedNodeMap
+
+    // condition when length == 0(details attribude open="") or when length == 1(details attribude open="")
+    if (length == 0) {
+        showItem(arrow, text)
+    } else {
+        hideItem(arrow, text)
+    }
+};
+
+addListeners(items, handleClick)
