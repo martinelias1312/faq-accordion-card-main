17232683cdfa482241bb5a87800b9b05a762c05d
smradupan <martinelias630@gmail.com>
2022-04-15 20:30:17 +0200

    functions put out of handleClick function

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,32 +11,35 @@ const addListeners = (array, callback) => {
     })
 };
 
+const showItem = (arrow, text) => {
+    text.style.fontWeight = `700`;
+    arrow.style.transform = `rotate(180deg)`;
+}
+
+const hideItem = (arrow, text) => {
+    text.style.fontWeight = `500`;
+    arrow.style.transform = `rotate(0deg)`;
+}
+
+// close other questions if any are oppened
+const closeOthers = (details, summary, svg) => {
+    details.forEach(item => item.open = false);
+    summary.forEach(item => item.style.fontWeight = `500`);
+    svg.forEach(item => item.style.transform = `rotate(0deg)`);
+}
+
 const handleClick = (e) => {
     // clicked selectors
     let clickedItem = e.target.closest('details');
     let arrow = clickedItem.querySelector('svg');
     let text = clickedItem.querySelector('summary');
-    
-    const showItem = (arrow, text) => {
-        text.style.fontWeight = `700`;
-        arrow.style.transform = `rotate(180deg)`;
-    }
-
-    const hideItem = (arrow, text) => {
-        text.style.fontWeight = `500`;
-        arrow.style.transform = `rotate(0deg)`;
-    }
-
-    const closeOthers = (details, summary, svg) => {
-        details.forEach(item => item.open = false);
-        summary.forEach(item => item.style.fontWeight = `500`);
-        svg.forEach(item => item.style.transform = `rotate(0deg)`);
-    }
 
     // condition when clicked item is closed then open it
     if (clickedItem.open == false) {
+
         closeOthers(allDetails, allSummary, allSvg);
         showItem(arrow, text);
+
     } else {
         hideItem(arrow, text);
     }
